993a90a62044ce6c19455f086800a3695625789f
Agustin DElia <agusrdelia@gmail.com>
2020-09-03 11:54:15 -0300

    Ejempos de ajax

--- atencionusuarios/page-slug.php
+++ atencionusuarios/page-slug.php
@@ -4,15 +4,12 @@
 	<?php get_template_part( 'template-parts/common/head' ); ?>
 </head>
 <body>
-    
-    <?php get_template_part( 'template-parts/common/header' ); ?>
+	<?php get_template_part( 'template-parts/common/header' ); ?>
 
-    
-    <?php get_template_part( 'template-parts/common/users/login-profile.php' ); ?>
+	<?php get_template_part( 'template-parts/users/login-profile' ); ?>
 
-    <?php get_template_part( 'template-parts/common/users/register-profile.php' ); ?>
+	<?php get_template_part( 'template-parts/users/register-profile' ); ?>
 
-	
 	<?php get_template_part( 'template-parts/common/footer' ); ?>
 	<?php get_template_part( 'template-parts/common/footerscripts' ); ?>
 </body>
@@ -21,3 +18,8 @@
 <?php
 /* Por algun motivo no me permite entrar desde localhost/page-login.php 
 Me redirije a index.php. 
+
+page-id.php
+page-{slug}.php
+page.php
+index.php
